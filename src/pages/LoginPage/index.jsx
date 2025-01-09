921e251bb7fac88d2ef8f18d390a8c678832175a
Mehmet Ardıç <ardicmehmet96@gmail.com>
2025-01-09 15:00:15 +0300

    test

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -5,6 +5,7 @@ export default function LoginPage() {
     <div className="min-h-screen relative overflow-hidden bg-gradient-to-br from-purple-900 via-purple-800 to-blue-900">
       <div className="min-h-screen flex items-center justify-center px-4">
         <LoginForm />
+        
       </div>
     </div>
   );
